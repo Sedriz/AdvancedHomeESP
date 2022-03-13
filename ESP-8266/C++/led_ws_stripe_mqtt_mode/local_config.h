4d6d5745d5970e572af038afc8178f436cb63c89
sedriz <sedrik.staack@gmx.de>
2022-03-13 12:21:44 +0100

    localconfig changes

--- ESP-8266/C++/led_ws_stripe_mqtt_mode/local_config.h
+++ ESP-8266/C++/led_ws_stripe_mqtt_mode/local_config.h
@@ -1,18 +1,24 @@
 // Wifi:
-#define h_wifi_ssid "<Your WIFI SSID>"
-#define h_wifi_password "<Your WIFI PASSWORD>"
+#define h_wifi_ssid "<Your Network SSID>"
+#define h_wifi_password "<Your Network Password>"
 
 // MQTT:
-#define h_mqttbroker_host "<The IP of the MQTT broker>"
-#define h_mqtt_user "<MQTT USERNAME>"
-#define h_mqtt_password "<MQTT Password>"
-#define h_publish_interval_s 100
+#define h_mqttbroker_host "<The IP of your MQTT broker>"
+#define h_mqtt_user "<The Username to connect to your MQTT broker>"
+#define h_mqtt_password "<The Password to conenct to your MQTT broker>"
 
 //Device:
-#define h_device_id "12"
-#define h_location "Partyzimmer"
+#define h_device_id "<Your Device ID. You have to register you device to (Smartboot) to get your ID>"
+#define h_location "<The location your Device is registered in>"
 
-//Only Programmable LED-Stripes:
-#define h_outputPin 1
-#define h_num_led 100
+//Stripe:
+#define LED_PIN 4 // The output pin where your LED-Stripe is connected. 4 -> D2
+#define NUM_LEDS 256 //The length of your Stripe 
+#define LED_TYPE WS2813 // The model of your LED-Stripe
+#define COLOR_ORDER GRB // RGB / GRB
+
+//Timeclient:
+#define NTP_OFFSET 60 * 60     // In seconds
+#define NTP_INTERVAL 60 * 1000 // In miliseconds
+#define NTP_ADDRESS "europe.pool.ntp.org"
 
